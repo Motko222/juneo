0180c9c18cd26de38b32918f835818ab5d517003
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-27 23:26:15 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+json=~/logs/report-$folder
 source ~/.bash_profile
 
 service=$(sudo systemctl status juneod --no-pager | grep "active (running)" | wc -l)
@@ -8,16 +11,12 @@ type="validator"
 network="mainnet"
 host=$(cat ~/juneogo/config.json | jq -r '."http-host"')
 chain=$(curl -sX POST --data '{ "jsonrpc":"2.0", "id" :1, "method" :"info.getNetworkName" }' -H 'content-type:application/json;' $host:9650/ext/info | jq -r .result.networkName)
-id=$JUNEO_ID
-group=node
-owner=$OWNER
-
 is_bootstrapped=$(curl -sX POST --data '{ "jsonrpc": "2.0", "id":1, "method":"info.isBootstrapped", "params": {"chain" : "JUNE" } }' \
    -H 'content-type: application/json; ' $host:9650/ext/info | jq .result.isBootstrapped)
-json=$(curl -sX POST --data '{ "jsonrpc":"2.0", "id" :1, "method" :"info.getNodeID" }' -H 'content-type:application/json' $host:9650/ext/info)
-node_id=$(echo $json | jq -r .result.nodeID)
-public_key=$(echo $json | jq -r .result.nodePOP.publicKey)
-proof_of_possession=$(echo $json | jq -r .result.nodePOP.proofOfPossession)
+status_json=$(curl -sX POST --data '{ "jsonrpc":"2.0", "id" :1, "method" :"info.getNodeID" }' -H 'content-type:application/json' $host:9650/ext/info)
+node_id=$(echo $status_json | jq -r .result.nodeID)
+public_key=$(echo $status_json | jq -r .result.nodePOP.publicKey)
+proof_of_possession=$(echo $status_json | jq -r .result.nodePOP.proofOfPossession)
 version=$(cat ~/.juneogo/logs/main.log | grep "initializing node" | tail -1 | awk -F "initializing node " '{print $NF}' | jq -r .version | awk -F "/" '{print $NF}')
 
 if [ $service -ne 1 ]
@@ -32,35 +31,30 @@ elif $is_bootstrapped
    message="not bootstrapped"
 fi
 
-cat << EOF
+cat >$json << EOF
 {
-  "id":"$id",
-  "machine":"$MACHINE",
-  "network":"$network"
-  "chain":"$chain",
-  "type":"node",
-  "version":"$version",
-  "status":"$status",
-  "message":"$message",
-  "service":$service,
-  "pid":$pid,
-  "is_bootstrapped":"$is_bootstrapped",
-  "node_id":"$node_id",
-  "public_key":"$public_key",
-  "proof_of_possession":"$proof_of_possession",
-  "updated":"$(date --utc +%FT%TZ)"
+  "updated":"$(date --utc +%FT%TZ)",
+  "measurement":"report",
+  "tags": {
+         "id":"$folder",
+         "machine":"$MACHINE",
+         "grp":"node",
+         "owner":"$OWNER"
+  },
+  "fields": {
+        "network":"$network"
+        "chain":"$chain",
+        "version":"$version",
+        "status":"$status",
+        "message":"$message",
+        "service":$service,
+        "pid":$pid,
+        "is_bootstrapped":"$is_bootstrapped",
+        "node_id":"$node_id",
+        "public_key":"$public_key",
+        "proof_of_possession":"$proof_of_possession"
+  }
 }
 EOF
 
-# send data to influxdb
-if [ ! -z $INFLUX_HOST ]
-then
- curl --request POST \
- "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$INFLUX_BUCKET&precision=ns" \
-  --header "Authorization: Token $INFLUX_TOKEN" \
-  --header "Content-Type: text/plain; charset=utf-8" \
-  --header "Accept: application/json" \
-  --data-binary "
-    report,id=$id,machine=$MACHINE,grp=$group,owner=$owner status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\",network=\"$network\" $(date +%s%N) 
-    "
-fi
+cat $json
