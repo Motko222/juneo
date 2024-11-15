4ba828301a2ff0df54d9fd130defb00e896f0c9d
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-15 21:35:49 +0100

    Create install-service.sh

---
+++ install-service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+
+tee /etc/systemd/system/$folder.service > /dev/null <<EOF 
+[Unit]
+Description=juneo node
+After=network.target
+
+[Service]
+User=root
+Type=simple
+ExecStart=/root/juneogo/juneogo --config-file="/root/juneogo/config.json"
+Restart=on-failure
+RestartSec=60
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable $folder
