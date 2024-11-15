fbec6183d8f4499951bc3b7d30a2a6ee8d5bf5e1
Motko <77466238+Motko222@users.noreply.github.com>
2024-11-15 21:38:16 +0100

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 
-sudo journalctl -u juneod.service -f --no-hostname -o cat
+sudo journalctl -u $folder.service -f --no-hostname -o cat
