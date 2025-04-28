b94bb26cc4cec2a8be4cdb95131b47154187fa7e
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-28 09:33:15 +0200

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -3,7 +3,7 @@
 read -p "Sure? " c
 case $c in y|Y) ;; *) exit ;; esac
 
-cd /root
+cd ~
 rm -r juneogo
 rm -r .juneogo/db
 rm -r .juneogo/plugins
@@ -14,7 +14,7 @@ chmod +x ~/juneogo-binaries/juneogo
 chmod +x ~/juneogo-binaries/plugins/jevm
 chmod +x ~/juneogo-binaries/plugins/srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e
 mkdir -p ~/.juneogo/plugins
-mv ~/juneogo-binaries/plugins/jevm ~/.juneogo/plugins
-mv ~/juneogo-binaries/plugins/srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e ~/.juneogo/plugins
+cp ~/juneogo-binaries/plugins/jevm ~/.juneogo/plugins
+cp ~/juneogo-binaries/plugins/srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e ~/.juneogo/plugins
 mkdir ~/juneogo
 cp ~/juneogo-binaries/juneogo ~/juneogo
